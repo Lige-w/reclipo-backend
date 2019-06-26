0150f7e454ec6f039307a3fcc8daaa5b5e016962
github username <github email address>
2019-06-26 13:52:55 -0400

    name change begin auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+
+  def secret_key
+    'S3kr12'
+  end
+
+  def encode_token(paylod)
+    JWT.encode(payload, secret_key)
+  end
+
+  def
+    JWT.decode(token, secret_key)[0]
+  end
 end
