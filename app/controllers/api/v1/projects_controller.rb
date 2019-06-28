74e0055de91f69f22e4249d8297aa947a5952433
github username <github email address>
2019-06-28 11:47:10 -0400

    added #create for project

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,4 +1,15 @@
 class Api::V1::ProjectsController < ApplicationController
   def create
+    @project = current_user.projects.build(project_params)
+    if @project.valid?
+      @project.save
+      render json: @project
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description)
   end
 end
