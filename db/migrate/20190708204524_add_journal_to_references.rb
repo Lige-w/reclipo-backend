6de9a96f5afcc85cba52226be73ba6d5ed134b8a
github username <github email address>
2019-07-08 16:52:44 -0400

    undo add journal to references

--- db/migrate/20190708204524_add_journal_to_references.rb
+++
@@ -1,5 +0,0 @@
-class AddJournalToReferences < ActiveRecord::Migration[5.2]
-  def change
-    add_column :references, :journal, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_08_204524) do
+ActiveRecord::Schema.define(version: 2019_07_03_205548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,7 +81,6 @@ ActiveRecord::Schema.define(version: 2019_07_08_204524) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "medium"
-    t.string "journal"
   end
 
   create_table "tags", force: :cascade do |t|
